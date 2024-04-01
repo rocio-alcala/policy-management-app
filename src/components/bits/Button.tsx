272688c10cdbb74c5c6c57934a6e2cee67b25c3d
Rocio Alcala <rro.alcala@gmail.com>
2024-04-01 13:05:49 -0300

    adding custom classname to button

--- src/components/bits/Button.tsx
+++ src/components/bits/Button.tsx
@@ -3,23 +3,27 @@ import clsx from "clsx/lite";
 interface ButtonSpecificPropsType {
   isLoading?: boolean;
   primary?: boolean;
+  className?: string;
 }
 
 export default function Button({
   children,
   isLoading,
   primary,
+  className,
   ...rest
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonSpecificPropsType) {
   const buttonColorClassName = clsx(
-    "w-full rounded disabled:bg-slate-400 p-3 text-center border-2 border-tertiary-default tracking-widest text-sm font-semibold leading-4",
-    primary && "bg-button-primary text-text-button-primary shadow-button",
-    !primary && "bg-transparent text-tertiary-default "
+    "w-full rounded disabled:bg-slate-400 p-3 text-center border-2  tracking-widest text-sm font-semibold leading-4",
+    primary &&
+      "bg-button-primary text-text-button-primary shadow-button border-tertiary-default",
+    !primary && "bg-transparent text-tertiary-default border-tertiary-default",
+    className
   );
 
   return (
-        <button className={buttonColorClassName} {...rest}>
-      {isLoading ? <p>LOADING</p> : children} <div className=""></div>
+    <button className={buttonColorClassName} {...rest}>
+      {isLoading ? <p>LOADING</p> : children}
     </button>
   );
 }
