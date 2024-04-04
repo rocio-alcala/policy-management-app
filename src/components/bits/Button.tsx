f9a1b6eb0ea4485ddcd5302b9bcf0aa6e3dbd5b5
Rocio Alcala <rro.alcala@gmail.com>
2024-04-03 23:12:35 -0300

    button on disable

--- src/components/bits/Button.tsx
+++ src/components/bits/Button.tsx
@@ -15,7 +15,7 @@ export default function Button({
   ...rest
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonSpecificPropsType) {
   const buttonColorClassName = cn(
-    "w-full rounded disabled:bg-slate-400 p-3 text-center border-2  tracking-widest text-sm font-semibold leading-4",
+    "w-full rounded disabled:opacity-50 p-3 text-center border-2  tracking-widest text-sm font-semibold leading-4",
     primary &&
       "bg-button-primary text-text-button-primary shadow-button border-tertiary-default",
     !primary && "bg-transparent text-tertiary-default border-tertiary-default",
