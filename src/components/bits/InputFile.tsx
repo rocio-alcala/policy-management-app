109c7f92e889909dab783f5169c117f0a9aca78f
Rocio Alcala <rro.alcala@gmail.com>
2024-07-26 12:08:36 -0300

    chore: input File

---
+++ src/components/bits/InputFile.tsx
@@ -0,0 +1,55 @@
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+import { MdFileUpload } from "react-icons/md";
+
+import { cn } from "../../utils/utils";
+
+import Errors from "./Errors";
+
+interface InputFileSpecificProps {
+  id: string | number;
+  description?: string;
+  errors?: string;
+  label?: string;
+}
+
+const InputFile = forwardRef<
+  HTMLInputElement,
+  ComponentPropsWithoutRef<"input"> & InputFileSpecificProps
+>(
+  (
+    { label, errors, required, description, className, children, ...props },
+    ref,
+  ) => {
+    return (
+      <div className={cn("flex-col mt-3", className)}>
+        <label>
+          {label && (
+            <legend className="font-semibold leading-6 py-1 text-grey8-dark-text">
+              {label}
+              {required && <span className="text-red-500">*</span>}
+            </legend>
+          )}
+          <input
+            className=" opacity-0 hidden"
+            ref={ref}
+            type="file"
+            aria-label={label}
+            {...props}
+          />
+          <div className="w-full text-axa-blue text-center relative gap-2 flex items-center justify-center rounded disabled:opacity-50 p-3 border-dashed border-2 text-sm font-semibold leading-4 bg-grey1">
+            <MdFileUpload size={20} className="fill-axa-blue" />
+            {children}
+          </div>
+        </label>
+        <Errors message={errors} />
+        {description && (
+          <div className="text-xs text-gray-400 tracking-wide leading-6">
+            {description}
+          </div>
+        )}
+      </div>
+    );
+  },
+);
+
+export default InputFile;
